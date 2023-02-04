b43879eda5b3a78ce76ff96004d083fa8fd3293a
Pavel <lovecars527@gmail.com>
2023-02-04 16:16:54 +0300

    initial operation gateway-service

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,13 +2,14 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableWebFluxSecurity
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
-
 }
