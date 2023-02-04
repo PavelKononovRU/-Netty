391ad609117cdf54c765f3d1ccf27022a8487462
Pavel <lovecars527@gmail.com>
2023-02-05 02:38:07 +0300

    Gateway works without dynamic routes

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,13 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-
 @SpringBootApplication
 @EnableWebFluxSecurity
-@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableCaching
 public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
--- gateway-service/src/main/java/com/example/gatewayservice/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/example/gatewayservice/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.example.gatewayservice.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+
+import java.util.concurrent.TimeUnit;
 
 
 @Configuration
@@ -52,5 +60,20 @@ public class SecurityConfig {
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:8080/api/user/home");
 
         return oidcLogoutSuccessHandler;
+    }
+
+    /*
+    * ИНИЦИАЛИЗАЦИЯ бинов КЭШа  Caffeine
+    * */
+    @Bean
+    public Caffeine<Object,Object> caffeineConfig() {
+        return Caffeine.newBuilder().expireAfterWrite(120, TimeUnit.MINUTES);
+    }
+    @Bean
+    @Primary
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
     }
 }
